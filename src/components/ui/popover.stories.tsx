651b9ae5efe778c121f4606697948d1d06eff4a7
Gui Rangel <guirangel.310@gmail.com>
2024-03-20 19:20:32 -0300

    Add popover

---
+++ src/components/ui/popover.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+import { Button } from "./button";
+
+const meta = {
+  title: "ui/Popover",
+  component: PopoverContent,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    align: {
+      control: { type: "radio" },
+      options: ["start", "center", "end"],
+      table: { type: { summary: "string" } },
+    },
+    sideOffset: {
+      type: "number",
+      default: 4,
+      description: "Offset in pixels",
+      table: { type: { summary: "number" } },
+    },
+  },
+  args: {
+    align: "center",
+    sideOffset: 4,
+  },
+} satisfies Meta<typeof PopoverContent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <Popover>
+      <PopoverTrigger>
+        <Button>Trigger</Button>
+      </PopoverTrigger>
+      <PopoverContent {...args}></PopoverContent>
+    </Popover>
+  ),
+  args: {
+    children: "Popover content",
+  },
+};
